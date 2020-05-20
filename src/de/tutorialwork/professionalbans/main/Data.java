bdc38dbbf47d808616ec532b5b5886b1fb9bd764
Manuel <tutorialworktv@gmail.com>
2020-05-20 21:32:11 +0200

    Create table only if not exits

--- src/de/tutorialwork/professionalbans/main/Data.java
+++ src/de/tutorialwork/professionalbans/main/Data.java
@@ -69,7 +69,7 @@ public class Data {
         try{
             PreparedStatement ps_acc = Main.mysql.getCon()
                     .prepareStatement(
-                            "CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, uuid VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, auth TINYINT(1) NOT NULL, authcode VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
+                            "CREATE TABLE user IF NOT EXISTS (id INT AUTO_INCREMENT NOT NULL, uuid VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, auth TINYINT(1) NOT NULL, authcode VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
                     );
             ps_acc.executeUpdate();
             ps_acc.close();
@@ -131,19 +131,19 @@ public class Data {
             ps_privatemsg.close();
             PreparedStatement ps_tokens = Main.mysql.getCon()
                     .prepareStatement(
-                            "CREATE TABLE tokens (id INT AUTO_INCREMENT NOT NULL, uuid VARCHAR(255) NOT NULL, token VARCHAR(255) NOT NULL, token_description VARCHAR(255) DEFAULT NULL, firebase_token VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
+                            "CREATE TABLE tokens IF NOT EXISTS (id INT AUTO_INCREMENT NOT NULL, uuid VARCHAR(255) NOT NULL, token VARCHAR(255) NOT NULL, token_description VARCHAR(255) DEFAULT NULL, firebase_token VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
                     );
             ps_tokens.executeUpdate();
             ps_tokens.close();
             PreparedStatement ps_setting = Main.mysql.getCon()
                     .prepareStatement(
-                            "CREATE TABLE setting (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, value VARCHAR(1000) DEFAULT NULL, UNIQUE INDEX UNIQ_9F74B8985E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
+                            "CREATE TABLE setting IF NOT EXISTS (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, value VARCHAR(1000) DEFAULT NULL, UNIQUE INDEX UNIQ_9F74B8985E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
                     );
             ps_setting.executeUpdate();
             ps_setting.close();
             PreparedStatement ps_invite = Main.mysql.getCon()
                     .prepareStatement(
-                            "CREATE TABLE invite (id INT AUTO_INCREMENT NOT NULL, code VARCHAR(255) NOT NULL, creator INT NOT NULL, creationdate DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
+                            "CREATE TABLE invite IF NOT EXISTS (id INT AUTO_INCREMENT NOT NULL, code VARCHAR(255) NOT NULL, creator INT NOT NULL, creationdate DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB"
                     );
             ps_invite.executeUpdate();
             ps_invite.close();
